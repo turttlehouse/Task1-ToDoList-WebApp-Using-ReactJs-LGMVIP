a165dec2e493c74a2b93b454193cdd929458b6a8
turttlehouse <buidandrun464@gmail.com>
2023-09-19 23:48:45 +0545

    TodoForm.js -Taking the props and using the props to call the method addTask and setting the input back to empty

--- mytodolistapp/src/Components/TodoForm.js
+++ mytodolistapp/src/Components/TodoForm.js
@@ -1,7 +1,7 @@
 import React ,{useState} from 'react'
 
 
-export default function TodoForm() {
+export default function TodoForm(props) {
 
        //Declaring the state named input
        const [input ,setInput] = useState('');  //if the user is writing something in the box then it will keep the record of that state
@@ -10,7 +10,18 @@ export default function TodoForm() {
         //avoids page reloads
         e.preventDefault();
 
-        
+        //This method will take the id parameters for now id is randomly generated and users need to pass one text that will be our task which we will be gettng from the input
+        //we also need to pass one boolean operator is the task completed or not
+        props.addTask({
+            id:Math.floor(Math.random()*10000),
+            text :input,
+            isComplete:false
+        })
+
+        //we need to set out input back to empty string
+        setInput('')
+
+
        }
 
   return (
