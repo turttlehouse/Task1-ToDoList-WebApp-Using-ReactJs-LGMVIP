cac42d526c33538739076ab1f776e6796d0bc030
turttlehouse <buidandrun464@gmail.com>
2023-09-20 07:52:19 +0545

    checking without destructuring the props

--- mytodolistapp/src/Components/Todo.js
+++ mytodolistapp/src/Components/Todo.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { RiCloseCircleLine }from 'react-icons/ri'
 import {TiTick} from 'react-icons/ti'
 
-export const Todo = ({todos,removeTask,completeTask}) => {
+
+//using props by destructing the props
+
+// export const Todo = ({todos,removeTask,completeTask}) => {  
+
+  //Or We can directly use the props without destructuring
+
+export const Todo = (props) => {
+  
 
     //Now we need to map over the todo's array and return a task from here
-    return todos.map ((todo,index)=>(
+    return props.todos.map ((todo,index)=>(
 
      
 
@@ -13,9 +21,9 @@ export const Todo = ({todos,removeTask,completeTask}) => {
         <div className='todo-text'>{todo.text}</div>
          <div className='icons' key={todo.id}>
 
-        <TiTick onClick={() => { completeTask(todos.id) } }></TiTick>
+        <TiTick onClick={() => { props.completeTask(todo.id) } }></TiTick>
 
-        <RiCloseCircleLine onClick={() => { removeTask(todo.id) } }></RiCloseCircleLine>
+        <RiCloseCircleLine onClick={() => { props.removeTask(todo.id) } }></RiCloseCircleLine>
 
 
       </div>
